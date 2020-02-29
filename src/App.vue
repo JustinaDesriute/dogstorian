2a59fd499f445fd80cfab962a11a9c88f55309a2
Justina Desriute <justinadesriute@Justinas-MBP.fritz.box>
2020-02-29 15:46:52 +0100

    Initial commit for the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/paw.png">
+    <HelloWorld msg="Welcome to Dogstorian"/>
   </div>
 </template>
 
